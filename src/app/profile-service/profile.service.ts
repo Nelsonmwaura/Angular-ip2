e1a5f8ce2673ae9fa781dfac520e7e905fa902d4
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-09-01 16:16:34 +0300

    Creating a service

---
+++ src/app/profile-service/profile.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/Http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  
+
+
+  constructor() {private http:Http }
+}
