abe560edcfc923611184cfb46ca4b8f6d24d24e6
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-09-02 15:36:45 +0300

    adding client id's

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -7,5 +7,17 @@ import 'rxjs/add/operator/map';
 })
 export class ProfileService {
 
-  
+  private username :string;
+  private clientid = 'Iv1.df9b6881ee98d126';
+  private clientsecret = '3e4cce66aa3d3255af2dd7e3ba8a07f046db55cb';
+
+
+  constructor(private http:Http ){
+    console.log ("service is now ready!");
+    this.username = 'Nelsonmwaura';
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username = "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
 }
